e8324a3d2e96139cb27943c54eca6531700d38f6
路洋(Joy) <393780933@qq.com>
2018-03-03 18:34:53 +1100

    modify spider using header and body

--- crawler/CHIIA/spiders/fectiva.py
+++ crawler/CHIIA/spiders/fectiva.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.http import Request
 
 class FectivaSpider(scrapy.Spider):
     name = 'fectiva'
     allowed_domains = ['factiva.com']
-    start_urls = ['https://global-factiva-com.virtual.anu.edu.au/ha/default.aspx']
+   
     
+    def start_requests(self):
+        post_data = "ftx=Australia"
+        search_url = 'https://global-factiva-com.virtual.anu.edu.au/ha/default.aspx'
+        post_header={}
+        post_header["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8" 
+        yield Request(url=search_url, method='POST',headers= post_header, body=post_data,
+                          callback=self.parse)
 
+    
     def parse(self, response):
         print(response.body)
         filename = 'index.html'
