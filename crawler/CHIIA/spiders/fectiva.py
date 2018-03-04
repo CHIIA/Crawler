1d2a82e477275d50339f4702a3b9718b698f4bc8
路洋(Joy) <393780933@qq.com>
2018-03-04 12:41:27 +1100

    use regrex to process article redirect post, need test

--- crawler/CHIIA/spiders/fectiva.py
+++ crawler/CHIIA/spiders/fectiva.py
@@ -4,7 +4,7 @@ from scrapy.http import Request
 from scrapy.http import FormRequest
 from bs4 import BeautifulSoup
 
-
+import re
 
 
 class FectivaSpider(scrapy.Spider):
@@ -19,10 +19,18 @@ class FectivaSpider(scrapy.Spider):
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         yield FormRequest(url=search_url,formdata={'ftx': 'Chin*'},
                           callback=self.parse_search)
-        
+
+    def parse_redirect(self,response):
+        redirect_url = 'https://global-factiva-com.virtual.anu.edu.au/ga/default.aspx'
+        key=re.search(r'doLinkPost\(\".*\,\"(.*)\,\"(.*)"\)',response.body.decode('utf-8'))
+        xformstate = key.group(1)
+        xformsesstate = key.group(2)
+        print(xformstate)
+        yield FormRequest(url=redirect_url,formdata={'_XFORMSTATE':xformstate ,'_XFORMSESSSTATE':xformsesstate},
+                          callback=self.parse_article)
     def parse_article(self,response):
         self.i+=1
-        filename = 'articles/%d.html' % i
+        filename = 'articles/%d.html' % self.i
         with open(filename, 'wb') as f:
             f.write(response.body)
 
@@ -35,6 +43,6 @@ class FectivaSpider(scrapy.Spider):
             title = item.string
             articles_url='https://global-factiva-com.virtual.anu.edu.au' + item.get('href')[2:]
             print('Title: {}\nURL: {}\n'.format(title,articles_url))
-            yield Request(url=articles_url,callback=self.parse_article,dont_filter=True)
+            yield Request(url=articles_url,callback=self.parse_redirect,dont_filter=True)
 
 
--- crawler/CHIIA/testsoup.py
+++ crawler/CHIIA/testsoup.py
@@ -1,3 +1,4 @@
+'''
 from bs4 import BeautifulSoup
 with open('logs/search.html','r') as f:
 	soup = BeautifulSoup(f.read(),"html.parser")
@@ -8,3 +9,13 @@ for item in soup.find_all("a","enHeadline"):
     print(item.string)
     end = url.rfind('/')
     print(url[:end])
+'''
+import re
+
+f = open(r'articles/1.html','rb')
+content = f.read().decode('utf-8')
+f.close()
+s=re.search(r'doLinkPost\(\".*\,\"(.*)\,\"(.*)"\)',content)
+
+
+print(s.group(1),'\n\n',s.group(2))
